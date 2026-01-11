496c4f03c20ad4a8784b60cb8589fd6ed67f7b04
Niklas Arnitz <narnitz@churchtools.de>
2026-01-11 17:03:12 +0100

    refactored RancilioS27ButtonMatrixHandler

---
+++ src/hardware/handlers/machine-specific-handlers/RancilioS27ButtonMatrixHandler.cpp
@@ -0,0 +1,36 @@
+#include "RancilioS27ButtonMatrixHandler.h"
+
+RancilioS27ButtonMatrixHandler::RancilioS27ButtonMatrixHandler(ButtonMatrixState *buttonMatrixState) : MachineButtonMatrixHandler(buttonMatrixState) {}
+
+void RancilioS27ButtonMatrixHandler::initializePins()
+{
+    pinMode(OUT_DOSE_BUTTONS, OUTPUT);
+    pinMode(OUT_TEA_WATER, OUTPUT);
+
+    // Dose 1
+    pinMode(IN_DOSE_1, INPUT_PULLUP);
+    // Dose 2
+    pinMode(IN_DOSE_2, INPUT_PULLUP);
+    // Dose 3
+    pinMode(IN_DOSE_3, INPUT_PULLUP);
+    // Dose 4
+    pinMode(IN_DOSE_4, INPUT_PULLUP);
+    // Continuous
+    pinMode(IN_CONTINUOUS, INPUT_PULLUP);
+
+    pinMode(IN_TEA_WATER, INPUT_PULLUP);
+
+    digitalWrite(OUT_DOSE_BUTTONS, LOW);
+    digitalWrite(OUT_TEA_WATER, LOW);
+}
+
+void RancilioS27ButtonMatrixHandler::handle()
+{
+    buttonMatrixState->groupOne.leftSingleDose = !digitalRead(IN_DOSE_1);
+    buttonMatrixState->groupOne.leftDoubleDose = !digitalRead(IN_DOSE_2);
+    buttonMatrixState->groupOne.continuous = !digitalRead(IN_CONTINUOUS);
+    buttonMatrixState->groupOne.rightSingleDose = !digitalRead(IN_DOSE_3);
+    buttonMatrixState->groupOne.rightDoubleDose = !digitalRead(IN_DOSE_4);
+
+    buttonMatrixState->tea = !digitalRead(IN_TEA_WATER);
+}
--- src/hardware/handlers/machine-specific-handlers/RancilioS27ButtonMatrixHandler.h
+++ src/hardware/handlers/machine-specific-handlers/RancilioS27ButtonMatrixHandler.h
@@ -7,65 +7,23 @@
 #include "Arduino.h"
 
 #define OUT_DOSE_BUTTONS FLEXIBLE_IO_7
+#define OUT_TEA_WATER FLEXIBLE_IO_9
+
 #define IN_DOSE_1 FLEXIBLE_IO_4
 #define IN_DOSE_2 FLEXIBLE_IO_5
 #define IN_DOSE_3 FLEXIBLE_IO_6
 #define IN_DOSE_4 FLEXIBLE_IO_1
 #define IN_CONTINUOUS FLEXIBLE_IO_2
+#define IN_TEA_WATER FLEXIBLE_IO_12
 
 class RancilioS27ButtonMatrixHandler : public MachineButtonMatrixHandler
 {
 public:
-    RancilioS27ButtonMatrixHandler(ButtonMatrixState *buttonMatrixState) : MachineButtonMatrixHandler(buttonMatrixState) {}
+    RancilioS27ButtonMatrixHandler(ButtonMatrixState *buttonMatrixState);
 
-    void initializePins()
-    {
-        pinMode(OUT_DOSE_BUTTONS, OUTPUT);
-        pinMode(OUT_TEA_WATER, OUTPUT);
+    void initializePins();
 
-        // Dose 1
-        pinMode(IN_DOSE_1, INPUT_PULLUP);
-        // Dose 2
-        pinMode(IN_DOSE_2, INPUT_PULLUP);
-        // Dose 3
-        pinMode(IN_DOSE_3, INPUT_PULLUP);
-        // Dose 4
-        pinMode(IN_DOSE_4, INPUT_PULLUP);
-        // Continuous
-        pinMode(IN_CONTINUOUS, INPUT_PULLUP);
-
-        pinMode(IN_TEA_WATER, INPUT_PULLUP);
-
-        digitalWrite(OUT_DOSE_BUTTONS, LOW);
-        digitalWrite(OUT_TEA_WATER, LOW);
-    }
-
-    void handle()
-    {
-        buttonMatrixState->groupOne.leftSingleDose = !digitalRead(IN_DOSE_1);
-        buttonMatrixState->groupOne.leftDoubleDose = !digitalRead(IN_DOSE_2);
-        buttonMatrixState->groupOne.continuous = !digitalRead(IN_CONTINUOUS);
-        buttonMatrixState->groupOne.rightSingleDose = !digitalRead(IN_DOSE_3);
-        buttonMatrixState->groupOne.rightDoubleDose = !digitalRead(IN_DOSE_4);
-
-        buttonMatrixState->tea = !digitalRead(IN_TEA_WATER);
-
-        // Logging ButtonMatrixState
-        // Serial.println("ButtonMatrixState:");
-        // Serial.printf("Group1: LSingle=%d, LDouble=%d, Cont=%d, RSingle=%d, RDouble=%d\n",
-        //               buttonMatrixState->groupOne.leftSingleDose,
-        //               buttonMatrixState->groupOne.leftDoubleDose,
-        //               buttonMatrixState->groupOne.continuous,
-        //               buttonMatrixState->groupOne.rightSingleDose,
-        //               buttonMatrixState->groupOne.rightDoubleDose);
-        // Serial.printf("Group2: LSingle=%d, LDouble=%d, Cont=%d, RSingle=%d, RDouble=%d\n",
-        //               buttonMatrixState->groupTwo.leftSingleDose,
-        //               buttonMatrixState->groupTwo.leftDoubleDose,
-        //               buttonMatrixState->groupTwo.continuous,
-        //               buttonMatrixState->groupTwo.rightSingleDose,
-        //               buttonMatrixState->groupTwo.rightDoubleDose);
-        // Serial.printf("Tea: %d\n", buttonMatrixState->tea);
-    }
+    void handle();
 };
 
 #endif // BREWPILOT_RANCILIOS27BUTTONMATRIXHANDLER_H
