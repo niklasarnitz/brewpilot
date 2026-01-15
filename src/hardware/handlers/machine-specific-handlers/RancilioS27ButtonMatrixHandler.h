1cdd54ffd65c8d092f70e1f538ff953ada04bdac
Niklas Arnitz <narnitz@churchtools.de>
2026-01-15 15:22:40 +0100

    replaced #define with constexpr

--- src/hardware/handlers/machine-specific-handlers/RancilioS27ButtonMatrixHandler.h
+++ src/hardware/handlers/machine-specific-handlers/RancilioS27ButtonMatrixHandler.h
@@ -6,15 +6,15 @@
 #include "../../../utils/noop.h"
 #include "Arduino.h"
 
-#define OUT_DOSE_BUTTONS FLEXIBLE_IO_7
-#define OUT_TEA_WATER FLEXIBLE_IO_9
+constexpr uint8_t OUT_DOSE_BUTTONS = FLEXIBLE_IO_7;
+constexpr uint8_t OUT_TEA_WATER = FLEXIBLE_IO_9;
 
-#define IN_DOSE_1 FLEXIBLE_IO_4
-#define IN_DOSE_2 FLEXIBLE_IO_5
-#define IN_DOSE_3 FLEXIBLE_IO_6
-#define IN_DOSE_4 FLEXIBLE_IO_1
-#define IN_CONTINUOUS FLEXIBLE_IO_2
-#define IN_TEA_WATER FLEXIBLE_IO_12
+constexpr uint8_t IN_DOSE_1 = FLEXIBLE_IO_4;
+constexpr uint8_t IN_DOSE_2 = FLEXIBLE_IO_5;
+constexpr uint8_t IN_DOSE_3 = FLEXIBLE_IO_6;
+constexpr uint8_t IN_DOSE_4 = FLEXIBLE_IO_1;
+constexpr uint8_t IN_CONTINUOUS = FLEXIBLE_IO_2;
+constexpr uint8_t IN_TEA_WATER = FLEXIBLE_IO_12;
 
 class RancilioS27ButtonMatrixHandler : public MachineButtonMatrixHandler
 {
