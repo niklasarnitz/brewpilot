417529275b05725278af49da942e77a012bf942e
Niklas Arnitz <narnitz@churchtools.de>
2026-01-15 15:24:16 +0100

    fixed another sonarqube problem

--- src/hardware/handlers/machine-specific-handlers/LaCimbaliM29ButtonMatrixHandler.h
+++ src/hardware/handlers/machine-specific-handlers/LaCimbaliM29ButtonMatrixHandler.h
@@ -1,6 +1,11 @@
 #ifndef BREWPILOT_LACIMBALIM29BUTTONMATRIXHANDLER_H
 #define BREWPILOT_LACIMBALIM29BUTTONMATRIXHANDLER_H
 
+#include "MachineButtonMatrixHandler.h"
+#include "../../../StaticConfig.h"
+#include "../../../utils/noop.h"
+#include "Arduino.h"
+
 constexpr uint8_t IN_GROUP_TWO_BIG_DRINKS = FLEXIBLE_IO_1;
 constexpr uint8_t IN_GROUP_TWO_SMALL_DRINKS = FLEXIBLE_IO_2;
 constexpr uint8_t IN_GROUP_TWO_STOP = FLEXIBLE_IO_3;
@@ -17,11 +22,6 @@ constexpr uint8_t OUT_GROUP_TWO_LEFT_HALF_AND_STOP = FLEXIBLE_IO_10;
 constexpr uint8_t OUT_GROUP_TWO_RIGHT_HALF = FLEXIBLE_IO_11;
 
 constexpr uint8_t IN_TEA_WATER = FLEXIBLE_IO_12;
-
-#include "MachineButtonMatrixHandler.h"
-#include "../../../StaticConfig.h"
-#include "../../../utils/noop.h"
-#include "Arduino.h"
 
 class LaCimbaliM29ButtonMatrixHandler : public MachineButtonMatrixHandler
 {
